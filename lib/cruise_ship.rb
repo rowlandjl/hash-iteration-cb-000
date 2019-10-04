301fb2162c4dd20187035e85d4fc68bc4ebfe09e
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 17:25:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -10,9 +10,9 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name 
-    end 
-  end 
+      winner = name
+    end
+  end
 
   winner
 end
